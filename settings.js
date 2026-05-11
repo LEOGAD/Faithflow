529151bcef4ff43fb9303e4fe3364d0e5743e66a
LEOGAD <60881161+LEOGAD@users.noreply.github.com>
2026-05-11 11:52:47 +0100

    Fix FOUC and unicode currency symbols

--- settings.js
+++ settings.js
@@ -1,3 +1,5 @@
+import { apiCall } from './app.js';
+
 const API_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:5000/api'
     : '/api';
